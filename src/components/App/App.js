32b77b4ceff35570d881d7db136f2058939af5a2
Alexandr Yakushkin <jakushkinn@yandex.ru>
2023-06-04 21:14:53 +0300

    Шаблон для роутов готов

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,21 @@
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
 import Header from '../Header/Header.js';
 import Main from '../Main/Main.js';
 import Footer from '../Footer/Footer.js';
+import './App.css';
 
 function App() {
   return (
     <div className="body">
       <div className="page">
         <Header onPath={'start'} />
-        <Main />
+        <Routes>
+          <Route path="/start" element={<Main />} />
+          <Route path="/order" element={<Main />} />
+          <Route path="/goods" element={<Main />} />
+          <Route path="/package" element={<Main />} />
+          <Route path="/end" element={<Main />} />
+        </Routes>
         <Footer />
       </div>
     </div>
