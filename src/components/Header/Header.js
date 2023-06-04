20b0a7cca1bdbc605fd98d5c93106a653cd756d6
Alexandr Yakushkin <yalex007@mail.ru>
2023-06-04 16:32:46 +0300

    Выравние элементов

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,7 +6,7 @@ export default function Header() {
   return (
     <header className="header">
       <nav className="header__nav">
-        <div className="header__burger-menu">
+        <div className="header__burger-menu transition">
           <span></span>
         </div>
         <div className="header__logo">
@@ -16,15 +16,17 @@ export default function Header() {
         </div>
       </nav>
       <h1 className="header__title">Упаковка</h1>
-      <div className="profile">
-        <div className="profile__name">sof-natgemokee</div>
-        <div className="profile__efficiency">
-          <img className="profile__img" alt="ракета" src={turbo}></img>
-          <p className="profile__efficiency-value">79%</p>
+      <div className="header__profile-section">
+        <div className="profile">
+          <div className="profile__name">sof-natgemokee</div>
+          <div className="profile__efficiency transition">
+            <img className="profile__img" alt="ракета" src={turbo}></img>
+            <p className="profile__efficiency-value">79%</p>
+          </div>
         </div>
-      </div>
-      <div className="header__func-menu">
-        <span></span>
+        <div className="header__func-menu transition">
+          <span></span>
+        </div>
       </div>
     </header>
   );
