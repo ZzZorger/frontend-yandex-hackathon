c90eb97b1a3cfb53497ea356e12bf4a373d53d6a
Alexandr Yakushkin <jakushkinn@yandex.ru>
2023-06-08 18:43:19 +0300

    Доработка страниц + роутинг между ними

--- src/components/OperationSelectorPage/OperationSelectorPage.jsx
+++ src/components/OperationSelectorPage/OperationSelectorPage.jsx
@@ -5,6 +5,8 @@ export default function OperationSelectorPage({ nextPage, operations, progress }
   return (
     <main className={style.Content}>
       <h2 className={style.Title}>Показатель за месяц</h2>
+      <p className={style.ProgressValue}>{progress.value}%</p>
+      <p className={style.ProgressPerHour}>{progress.operationsNum} операций в час</p>
       <progress className={style.Progress} max={progress.maxValue} value={progress.value}>
         {progress.operationsNum}
       </progress>
