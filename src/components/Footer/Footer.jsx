76c77399dbb76baf8d7bdacfa6cd7ceeebefadcc
Alexandr Yakushkin <jakushkinn@yandex.ru>
2023-06-08 20:29:51 +0300

    Добавление footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,3 +1,11 @@
+import style from './Footer.module.css';
+
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer className={style.Content}>
+      <p className={style.Adress}>Коммерческая тайна ООО «Яндекс», 119021, Россия, г. Москва, ул. Льва Толстого, д. 16</p>
+      <p className={style.Version}>2022.11.1 / 2023.1.68</p>
+      <p className={style.Copyright}>© 2003–2023 ООО «Яндекс»</p>
+    </footer>
+  );
 }
