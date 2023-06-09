b3d74bb42c352b063d0579cbd45558c83bde1f42
Valeria <Koroleva.Valeria.97@yandex.ru>
2023-06-09 14:31:08 +0300

    создание подсказок

--- src/components/ListItemProducts/ListItemProducts.jsx
+++ src/components/ListItemProducts/ListItemProducts.jsx
@@ -1,13 +1,61 @@
 import style from './ListItemProducts.module.css';
+import IMEI from '../HintЕlement/image/IMEI.svg';
+import Mark from '../HintЕlement/image/Mark.svg';
+import HintЕlement from '../HintЕlement/HintЕlement';
+
+// Используемые подсказки
+const bubbleWrap = {
+  text: 'Пузырчатая плёнка',
+  style: {
+    backgroundColor: '#E0EEFF',
+  },
+};
+const stretch = {
+  text: 'Стрейтч-плёнка',
+  style: {
+    backgroundColor: '#FFECCC',
+  },
+};
+// Подсказки которые будут на проде( в наших данных такой информации нет)
+const pack = {
+  text: 'Пакет',
+  style: {
+    backgroundColor: '#FFECCC',
+  },
+};
+const imei = {
+  text: 'Отсканируйте IMEI',
+  img: IMEI,
+  style: {
+    backgroundColor: '#FFD9DB',
+  },
+};
+const mark = {
+  text: 'Отсканируйте IMEI',
+  img: Mark,
+  style: {
+    backgroundColor: '#FFD9DB',
+  },
+};
 
 export default function ListItemProducts() {
   return (
     <li className={style.productItem}>
       <div className={style.productImage}>
         <img src={'/'} alt="Изображение товара для сканирования"></img>
-        <div className={style.productCounter}>{`${2} шт.`}</div>
+        <div className={style.productCounter}>{`${1} шт.`}</div>
       </div>
-      <p className={style.productText}>Умные часы Apple Watch Series 7 45 мм Aluminium Case, (PRODUCT)RED</p>
+
+      <div className={style.productDescription}>
+        {/* Размер блока + стили будут зависять от флагов с бека */}
+        <p className={style.productText}>
+          {
+            'Умные часы Apple Watch Series 7 45 мм Aluminium Case, (PRODUCT)RED Умные часы Apple Watch Series 7 45 мм Aluminium Case, (PRODUCT)RED'
+          }
+        </p>
+        {/* Условная отрисовка этого компонента, данные для компонента выше, стили зависят от флага, который придет с бека */}
+        <HintЕlement styles={bubbleWrap.style} text={bubbleWrap.text} />
+      </div>
     </li>
   );
 }
