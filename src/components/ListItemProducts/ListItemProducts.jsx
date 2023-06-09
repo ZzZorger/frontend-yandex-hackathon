e42352f1141920fc2347988b794d5fb5360da6d8
Valeria <Koroleva.Valeria.97@yandex.ru>
2023-06-09 14:53:16 +0300

    создание состояния по клику на товар

--- src/components/ListItemProducts/ListItemProducts.jsx
+++ src/components/ListItemProducts/ListItemProducts.jsx
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import style from './ListItemProducts.module.css';
 import IMEI from '../HintЕlement/image/IMEI.svg';
 import Mark from '../HintЕlement/image/Mark.svg';
@@ -39,8 +40,19 @@ const mark = {
 };
 
 export default function ListItemProducts() {
+  const [isActive, setIsActive] = useState(false);
+
+  const targetsStyle = () => {
+    setIsActive((current) => !current);
+  };
   return (
-    <li className={style.productItem}>
+    <li
+      className={style.productItem}
+      onClick={targetsStyle}
+      style={{
+        backgroundColor: isActive ? '#F3F0E9' : '',
+      }}
+    >
       <div className={style.productImage}>
         <img src={'/'} alt="Изображение товара для сканирования"></img>
         <div className={style.productCounter}>{`${1} шт.`}</div>
@@ -48,10 +60,14 @@ export default function ListItemProducts() {
 
       <div className={style.productDescription}>
         {/* Размер блока + стили будут зависять от флагов с бека */}
-        <p className={style.productText}>
-          {
-            'Умные часы Apple Watch Series 7 45 мм Aluminium Case, (PRODUCT)RED Умные часы Apple Watch Series 7 45 мм Aluminium Case, (PRODUCT)RED'
-          }
+        <p
+          className={style.productText}
+          // Добавится поле проверки с бека на наличие подсказки
+          style={{
+            display: isActive ? 'block' : '-webkit-box',
+          }}
+        >
+          {'Умные часы Apple Watch Series 7 45 мм Aluminium Case, (PRODUCT)RED'}
         </p>
         {/* Условная отрисовка этого компонента, данные для компонента выше, стили зависят от флага, который придет с бека */}
         <HintЕlement styles={bubbleWrap.style} text={bubbleWrap.text} />
