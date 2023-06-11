f04bbd235acf4ed7a3ddf8184e4c47296da8722d
Valeria <Koroleva.Valeria.97@yandex.ru>
2023-06-11 13:59:00 +0300

    Поправлена страница ScanProductPage

--- src/components/ScanProductPage/ScanProductPage.jsx
+++ src/components/ScanProductPage/ScanProductPage.jsx
@@ -1,15 +1,17 @@
 import style from './ScanProductPage.module.css';
-
 import MainButton from '../MainButton/MainButton';
 import ScanProduct from '../ScanProduct/ScanProduct';
-import TextItem from '../TextItem/TextItem';
+import BottomMenu from '../BottomMenu/BottomMenu';
 
 export default function ScanProductPage({ nextPage }) {
   return (
     <>
-      <MainButton text={'Есть проблема'} />
-      <ScanProduct />
-      <MainButton styles={{ background: 'var(--active-bg-elem)' }} text={`Заказ собран`} nextPage={nextPage} />
+      <div className={style.ScanProductPage}>
+        <MainButton text={'Есть проблема'} />
+        <ScanProduct />
+        <MainButton styles={{ background: 'var(--active-bg-elem)' }} text={`Заказ собран`} nextPage={nextPage} />
+      </div>
+      <BottomMenu />
     </>
   );
 }
