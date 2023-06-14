81f0e0a5c836bc37c92358e43d8155caeb0ba0b1
Alexandr Yakushkin <jakushkinn@yandex.ru>
2023-06-14 20:41:48 +0300

    Правка прогресса

--- src/pages/OperationSelectorPage/OperationSelectorPage.jsx
+++ src/pages/OperationSelectorPage/OperationSelectorPage.jsx
@@ -5,11 +5,11 @@ export default function OperationSelectorPage({ nextPage, operations, progress }
   return (
     <main className={style.Content}>
       <h2 className={style.Title}>Показатель за месяц</h2>
-      <p className={style.ProgressValue}>{progress.value}%</p>
+      {/* <p className={style.ProgressValue}>{progress.value}%</p>
       <p className={style.ProgressPerHour}>{progress.operationsNum} операций в час</p>
       <progress className={style.Progress} max={progress.maxValue} value={progress.value}>
         {progress.operationsNum}
-      </progress>
+      </progress> */}
       <ButtonsSet nextPage={nextPage} set={operations} />
     </main>
   );
