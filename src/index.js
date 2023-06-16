711ee0fc7cbb22d7cbf2df07582f646036465c00
Valeria <Koroleva.Valeria.97@yandex.ru>
2023-06-16 09:41:40 +0300

    подключение redux

--- src/index.js
+++ src/index.js
@@ -4,12 +4,21 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './components/App/App';
 import reportWebVitals from './reportWebVitals';
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
+import { rootReducer } from './services/reducers/index';
+
+export const store = configureStore({
+  reducer: rootReducer,
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>,
 );
