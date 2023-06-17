820184a35ed1d2505b5b931ff4ed0d8b4941b124
Alexandr Yakushkin <yalex007@mail.ru>
2023-06-18 02:43:55 +0300

    фикс обработки входных данных первыми страницами

--- src/components/ChoiсeButton/ChoiсeButton.jsx
+++ src/components/ChoiсeButton/ChoiсeButton.jsx
@@ -1,17 +1,22 @@
 import style from './ChoiсeButton.module.css';
 import { Link } from 'react-router-dom';
-// Маленькая кнопка на первых двух экранах
 
-export default function ChoiсeButton({ styles, text, route, handlePopupOpen, status }) {
-  if (status === 'busy') {
+export default function ChoiсeButton({ styles, text, route, handlePopupOpen, available }) {
+  if (!available) {
     styles = { background: '#F3F0E9', opacity: '0.5' };
   }
 
   return (
     <Link to={route}>
-      <button style={styles} className={`${style.ChoiсeButton}`} onClick={handlePopupOpen}>
-        {text}
-      </button>
+      {!available ? (
+        <button style={styles} className={`${style.ChoiсeButton}`} onClick={handlePopupOpen} disabled>
+          {text}
+        </button>
+      ) : (
+        <button style={styles} className={`${style.ChoiсeButton}`} onClick={handlePopupOpen}>
+          {text}
+        </button>
+      )}
     </Link>
   );
 }
