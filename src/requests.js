eeaa38be1d4daafa114b56d6fa4fb0a4b3ac5f30
Valeria <Koroleva.Valeria.97@yandex.ru>
2023-06-18 21:43:11 +0300

    докер, роуты + проблемы

--- src/requests.js
+++ src/requests.js
@@ -11,7 +11,15 @@ export default function postBox(productInNewBox, productInMainBox) {
   //   productInMainBox.push({ sku: product[0].sku, packaging_number: 1 });
   //   console.log(productInMainBox);
   //   console.log(productInNewBox);
-  // fetch('api/login/')
+  // fetch('http://127.0.0.1:8000/api/auth/login/', {
+  //   method: 'POST',
+  //   headers: {
+  //     'Content-Type': 'application/json;charset=utf-8',
+  //   },
+  //   body: JSON.stringify({
+  //     confirmation_code: 'ad481436-a8da-467a-96fc-167e3e999fb5',
+  //   }),
+  // })
   //   .then((response) => response.json())
   //   .then((commits) => console.log(commits));
 }
